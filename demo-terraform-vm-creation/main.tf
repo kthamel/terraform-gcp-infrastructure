4de5a5a4878728c3f0db5ddc271d92eb0108afd8
Kushan Thamel <kushan.thamel@gmail.com>
2023-10-18 21:58:01 +0100

    vm creation with ephemeral ip configuration was added

---
+++ demo-terraform-vm-creation/main.tf
@@ -0,0 +1,18 @@
+resource "google_compute_instance" "kthamel-instance" {
+  name         = "kthamel-demo-instance"
+  project      = "terraform-gcp-infrastructure"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+  }
+  labels = {
+    name    = "kthamel-terraform-gcp-instance"
+    project = "kthamel-terraform-gcp"
+  }
+}
