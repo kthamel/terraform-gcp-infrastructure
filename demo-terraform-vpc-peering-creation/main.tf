1839f7aa48921caabbd9d195391153dd2f9286c1
Kushan Thamel <kushan.thamel@gmail.com>
2023-11-08 23:43:13 +0000

    vpc peering connection configurations were added

---
+++ demo-terraform-vpc-peering-creation/main.tf
@@ -0,0 +1,103 @@
+resource "google_compute_network" "kthamel-vpc-dev" {
+  name                    = "kthamel-vpc-dev"
+  project                 = "terraform-gcp-infrastructure"
+  auto_create_subnetworks = true
+  mtu                     = 1600
+}
+
+resource "google_compute_network" "kthamel-vpc-test" {
+  name                    = "kthamel-vpc-test"
+  project                 = "terraform-gcp-infrastructure"
+  auto_create_subnetworks = true
+  mtu                     = 1600
+}
+
+resource "google_compute_subnetwork" "kthamel-vpc-dev-subnet-public" {
+  name          = "kthamel-vpc-dev-subnet-public"
+  ip_cidr_range = "172.16.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.kthamel-vpc-dev.id
+  project       = google_compute_network.kthamel-vpc-dev.project
+}
+
+resource "google_compute_subnetwork" "kthamel-vpc-test-subnet-public" {
+  name          = "kthamel-vpc-test-subnet-public"
+  ip_cidr_range = "172.17.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.kthamel-vpc-test.id
+  project       = google_compute_network.kthamel-vpc-test.project
+}
+
+resource "google_compute_firewall" "kthamel-vpc-dev-firewall-icmp" {
+  name     = "kthamel-vpc-dev-firewall-icmp"
+  network  = google_compute_network.kthamel-vpc-dev.name
+  project  = google_compute_network.kthamel-vpc-dev.project
+  priority = 100
+
+  allow {
+    protocol = "icmp"
+  }
+  source_tags = ["kthemel-dev"]
+}
+
+resource "google_compute_firewall" "kthamel-vpc-test-firewall-icmp" {
+  name     = "kthamel-vpc-test-firewall-icmp"
+  network  = google_compute_network.kthamel-vpc-test.name
+  project  = google_compute_network.kthamel-vpc-test.project
+  priority = 100
+
+  allow {
+    protocol = "icmp"
+  }
+  source_tags = ["kthemel-test"]
+}
+
+resource "google_compute_instance" "kthamel-instance-dev" {
+  name         = "kthamel-demo-instance-dev"
+  project      = "terraform-gcp-infrastructure"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.kthamel-vpc-dev.id
+  }
+  labels = {
+    name    = "kthamel-terraform-gcp-instance-dev"
+    project = "kthamel-terraform-gcp"
+  }
+}
+
+resource "google_compute_instance" "kthamel-instance-test" {
+  name         = "kthamel-demo-instance-test"
+  project      = "terraform-gcp-infrastructure"
+  machine_type = "e2-micro"
+  zone         = "us-west1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.kthamel-vpc-test.id
+  }
+  labels = {
+    name    = "kthamel-terraform-gcp-instance-test"
+    project = "kthamel-terraform-gcp"
+  }
+}
+
+resource "google_compute_network_peering" "kthamel-vpc-dev-peering" {
+  name         = "kthamel-vpc-dev-peering"
+  network      = google_compute_network.kthamel-vpc-dev.self_link
+  peer_network = google_compute_network.kthamel-vpc-test.self_link
+}
+
+resource "google_compute_network_peering" "kthamel-vpc-test-peering" {
+  name         = "kthamel-vpc-test-peering"
+  network      = google_compute_network.kthamel-vpc-test.self_link
+  peer_network = google_compute_network.kthamel-vpc-dev.self_link
+}
