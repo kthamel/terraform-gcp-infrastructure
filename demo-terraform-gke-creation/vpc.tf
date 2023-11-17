6cb1113b1fffb250fa732b0fccaeef459ebfe15e
Kushan Thamel <kushan.thamel@gmail.com>
2023-11-17 12:24:50 +0000

    gke creation configuration was added

--- demo-terraform-gke-creation/vpc.tf
+++ demo-terraform-gke-creation/vpc.tf
@@ -11,30 +11,16 @@ resource "google_compute_subnetwork" "kthamel-vpc-dev-subnet-1" {
   region        = "us-central1"
   network       = google_compute_network.kthamel-vpc-dev.self_link
   project       = google_compute_network.kthamel-vpc-dev.project
-}
 
-resource "google_compute_subnetwork" "kthamel-vpc-test-subnet-2" {
-  name          = "kthamel-vpc-dev-subnet-2"
-  ip_cidr_range = "172.17.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.kthamel-vpc-dev.self_link
-  project       = google_compute_network.kthamel-vpc-dev.project
-}
+  secondary_ip_range {
+    range_name    = "pods"
+    ip_cidr_range = "172.17.0.0/16"
+  }
 
-resource "google_compute_subnetwork" "kthamel-vpc-dev-subnet-3" {
-  name          = "kthamel-vpc-dev-subnet-3"
-  ip_cidr_range = "172.18.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.kthamel-vpc-dev.self_link
-  project       = google_compute_network.kthamel-vpc-dev.project
-}
-
-resource "google_compute_subnetwork" "kthamel-vpc-test-subnet-4" {
-  name          = "kthamel-vpc-dev-subnet-4"
-  ip_cidr_range = "172.19.0.0/16"
-  region        = "us-central1"
-  network       = google_compute_network.kthamel-vpc-dev.self_link
-  project       = google_compute_network.kthamel-vpc-dev.project
+  secondary_ip_range {
+    range_name    = "svcs"
+    ip_cidr_range = "172.18.0.0/16"
+  }
 }
 
 resource "google_compute_firewall" "kthamel-vpc-dev-firewall-tcp-all" {
