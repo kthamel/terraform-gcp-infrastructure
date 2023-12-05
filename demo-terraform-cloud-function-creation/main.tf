03d607ff1bcd8ffbdfe1e911add14853d9a7a7f8
Kushan Thamel <kushan.thamel@gmail.com>
2023-12-05 07:16:47 +0000

    removed the nodejs config files

---
+++ demo-terraform-cloud-function-creation/main.tf
@@ -0,0 +1,26 @@
+resource "google_storage_bucket" "kthamel-storage-bucket-dev" {
+  name     = "kthamel-storage-dev"
+  location = "us-central1"
+  project  = "terraform-gcp-infrastructure"
+}
+
+resource "google_storage_bucket_object" "kthamel-storage-object" {
+  name   = "code.zip"
+  bucket = google_storage_bucket.kthamel-storage-bucket-dev.name
+  source = "./code.zip"
+}
+
+resource "google_cloudfunctions_function" "kthamel-dev-function" {
+  name                  = "app"
+  region                = "us-central1"
+  project               = "terraform-gcp-infrastructure"
+  runtime               = "nodejs18"
+  available_memory_mb   = "256"
+  trigger_http          = true
+  source_archive_bucket = google_storage_bucket.kthamel-storage-bucket-dev.name
+  source_archive_object = google_storage_bucket_object.kthamel-storage-object.name
+}
+
+output "cloud-function-url" {
+  value = google_cloudfunctions_function.kthamel-dev-function.https_trigger_url
+}
